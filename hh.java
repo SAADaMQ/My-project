00d8068853bd7de5cefc0b90151d470d0a3f50d3
saad_MQ <144943692+SAADaMQ@users.noreply.github.com>
2024-10-09 22:39:51 +0300

    Update hh.java

--- hh.java
+++ hh.java
@@ -3,7 +3,7 @@ public class hh {
 
     public static void main(String[] args) {
         // Prints "Hello, World!" to the console
-        System.out.("Hello, World!");
+        System.out.println("Hello, World!");
     }
 }
 
